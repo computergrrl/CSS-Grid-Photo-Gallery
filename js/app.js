b22572a4024dfbf38c2f22cb5b44ded7937c3a96
Amber Stevens <anani.mowse@gmail.com>
2020-11-24 16:37:40 -0800

    Initial project layout & styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 //baguetteBox.run('.gallery');
 
-const search = new Filter('search', 'data-caption');
+// const search = new Filter('search', 'data-caption');
+
+const gallery = document.querySelector('#gallery');
+
+for (let i=1; i<=12; i++) {
+	gallery.innerHTML +=
+	`<a href="photos/thumbnails/${i}.jpg"><img src="photos/thumbnails/${i}.jpg"></a>`
+}
