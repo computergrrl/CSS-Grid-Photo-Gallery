060dcafdff11b325564ccc6ccc76cabe8433f770
Amber Stevens <anani.mowse@gmail.com>
2020-12-12 13:09:44 -0800

    Code comments & file cleanup

--- js/app.js
+++ js/app.js
@@ -6,23 +6,13 @@ function photoSearch() {
     const userInput = search.value.toLowerCase();
     const gallery = document.querySelectorAll(".image");
 
-    if (userInput == "" || userInput == " ") {
-        for (let i = 0; i < gallery.length; i++) {
-            gallery[i].style.display = " ";
-        }
+    //loop through gallery 
+    for (let i = 0; i < gallery.length; i++) {
+        let caption = gallery[i].getAttribute("data-caption");
 
-    } else {
+        //if input in search box is empty show all pics, else show only pic which caption matches inputValue
+        gallery[i].style.display = !userInput.trim() || caption.toLowerCase().includes(userInput) ? "inherit" : "none";
 
-        //loop through gallery 
-        for (let i = 0; i < gallery.length; i++) {
-            let caption = gallery[i].getAttribute("data-caption");
-
-            if (caption.toLowerCase().indexOf(userInput) > -1) {
-                gallery[i].style.display = " ";
-            } else {
-                gallery[i].style.display = "none";
-            }
-        }
     }
-}
 
+}
