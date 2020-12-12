81ce995eea399b47371f2c9b1a5f4c2ae5182029
Amber Stevens <anani.mowse@gmail.com>
2020-12-12 11:33:12 -0800

    failed attempt at image re-add

--- js/app.js
+++ js/app.js
@@ -5,16 +5,24 @@ function photoSearch() {
     const search = document.getElementById("search");
     const userInput = search.value.toLowerCase();
     const gallery = document.querySelectorAll(".image");
-    // console.log(gallery);
 
-    //loop through gallery a
-    for (let i=0; i < gallery.length; i++) {
-        let caption = gallery[i].getAttribute("data-caption");
+    if (userInput == "" || userInput == " ") {
+        for (let i = 0; i < gallery.length; i++) {
+            gallery[i].style.display = " ";
+        }
 
-          if(caption.toLowerCase().indexOf(userInput) > -1) {
-              gallery[i].style.display = " ";
-          } else {
-              gallery[i].style.display = "none";
-          }
+    } else {
+
+        //loop through gallery 
+        for (let i = 0; i < gallery.length; i++) {
+            let caption = gallery[i].getAttribute("data-caption");
+
+            if (caption.toLowerCase().indexOf(userInput) > -1) {
+                gallery[i].style.display = " ";
+            } else {
+                gallery[i].style.display = "none";
+            }
+        }
     }
 }
+
