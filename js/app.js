af5e7b082ac5a88c043028996e1ef9b1007c520d
Amber Stevens <anani.mowse@gmail.com>
2020-12-11 22:21:52 -0800

    Stuck on search function...SOO close (maybe?)

--- js/app.js
+++ js/app.js
@@ -5,5 +5,16 @@ function photoSearch() {
     const search = document.getElementById("search");
     const userInput = search.value.toLowerCase();
     const gallery = document.querySelectorAll(".image");
-    console.log(gallery);
+    // console.log(gallery);
+
+    //loop through gallery a
+    for (let i=0; i < gallery.length; i++) {
+        let randomVariableOnceIFigureItOut = "SOME VALUE HERE";
+
+          if(randomVariableOnceIFigureItOut.toLowerCase().indexOf(userInput) > -1) {
+              gallery[i].style.display = " ";
+          } else {
+              gallery[i].display.style= "none";
+          }
+    }
 }
