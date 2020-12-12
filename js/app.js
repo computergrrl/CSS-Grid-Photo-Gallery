34da2d07bcd047bbce4d615beae079123692da85
Amber Stevens <anani.mowse@gmail.com>
2020-12-11 21:42:06 -0800

    Removed break in a-tags & started search function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
-//baguetteBox.run('.gallery');
-
-// const search = new Filter('search', 'data-caption');
-
-// const gallery = document.querySelector('#gallery');
-
-// for (let i=1; i<=12; i++) {
-// 	gallery.innerHTML +=
-// 	`<a href="photos/thumbnails/${i}.jpg"><img src="photos/thumbnails/${i}.jpg"></a>`
-// }
-
-
 baguetteBox.run('.gallery');
+
+function photoSearch() {
+    //declare variables
+    const search = document.getElementById("search");
+    const userInput = search.value.toLowerCase();
+    const gallery = document.querySelectorAll(".image");
+    console.log(gallery);
+}
